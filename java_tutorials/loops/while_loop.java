606b8cd1f688889f4474011d4ab93fce3498a503
Vinayak Sharma <24100110112.uset@ltsu.ac.in>
2025-02-21 20:36:22 -0800

    Create while_loop.java

---
+++ java_tutorials/loops/while_loop.java
@@ -0,0 +1,13 @@
+package java_tutorials.loops;
+
+public class while_loop {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 5) {
+            System.out.println(num);
+            num += 1;
+        }
+
+    }
+
+}
