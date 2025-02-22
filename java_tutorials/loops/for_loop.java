2e97487e0b2c1a77dd6be8623910564c9e2ea01a
Vinayak Sharma <24100110112.uset@ltsu.ac.in>
2025-02-21 20:32:21 -0800

    Update for_loop.java

---
+++ java_tutorials/loops/for_loop.java
@@ -0,0 +1,10 @@
+package java_tutorials.loops;
+
+class for_loop {
+public static void main(String[] args) {
+    int num = 5;
+    for(int i=0;i<=num;i++){
+        System.out.println(i);
+        }
+    }
+}
