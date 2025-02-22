a212929eaa55d22fe183f799e01c6621ecf343c0
Vinayak Sharma <24100110112.uset@ltsu.ac.in>
2025-02-21 20:22:54 -0800

    Update input_tut3.java

---
+++ java_tutorials/Input_output/input_tut3.java
@@ -0,0 +1,15 @@
+//import Scanner class
+import java.util.Scanner;
+class input_tut3 {
+public static void main(String[] args) {
+    //create object of Scanner class
+    Scanner obj = new Scanner(System.in);
+    //output line
+    System.out.print("Enter the Number: ");
+    int num = obj.nextInt();
+    //use num
+    System.out.println("The Number entered: "+num);
+    //close Scanner Object
+    obj.close();
+    }
+}
